56eb728c8f3f0098617b75ab6f64990b2f8099c8
ALEXANDRE-NT\Alexandre Almeida <asa-008@hotmail.com.br>
2020-04-30 20:51:24 -0300

    funções

--- conteudo/js/main.js
+++ conteudo/js/main.js
@@ -1,15 +1,43 @@
 jQuery(document).ready(function($) {
     //Variáveis
+    var $elTipo = $("#tipo");
+    var $elNome= $("#nome");
+    var $elQuantidade= $("#quantidade");
+    var $elPreco= $("#preco");
+    var $elLitragem= $("#litragem");
+
     this.$btnAdicionar = $("#botao-adicionar");
+    this.$btnLimpar = $("#btnLimpar");
+    this.$btnCalcular = $("#btnCalcular");
 
     //Funções
     var adicioneItem = function () {
-        alert('teste');
+        alert('adicionar');
     };
 
+    var calcular = function () {
+        alert('calcular');
+    };
+
+    var limpeTela = function () {
+        $elNome.val("");
+        $elQuantidade.val("");
+        $elPreco.val("");
+        $elLitragem.val("");
+    };
+
+    //Eventos
     this.$btnAdicionar.on("click", function (e) {
         adicioneItem();
         e.stopPropagation();
     });
-    //Eventos
+
+    this.$btnLimpar.on("click", function () {
+        limpeTela();
+    });
+
+    this.$btnCalcular.on("click", function () {
+        limpeTela();
+        e.stopPropagation();
+    });
 });
