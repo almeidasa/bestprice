caa2528074734d8d64fd699e923c7df322d5fbf7
ALEXANDRE-NT\Alexandre Almeida <asa-008@hotmail.com.br>
2020-04-30 20:19:50 -0300

    Correções responsividade

--- conteudo/js/main.js
+++ conteudo/js/main.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function($) {
+    //Variáveis
+    this.$btnAdicionar = $("#botao-adicionar");
 
+    //Funções
+    var adicioneItem = function () {
+        alert('teste');
+    };
 
+    this.$btnAdicionar.on("click", function (e) {
+        adicioneItem();
+        e.stopPropagation();
+    });
+    //Eventos
 });
