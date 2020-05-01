38d0771a3fa7ee6f7011fe72ae2d939ce17347c6
ALEXANDRE-NT\Alexandre Almeida <asa-008@hotmail.com.br>
2020-04-30 22:53:39 -0300

    Adicionando e excluindo card

--- conteudo/js/main.js
+++ conteudo/js/main.js
@@ -6,13 +6,16 @@ jQuery(document).ready(function($) {
     var $elPreco= $("#preco");
     var $elLitragem= $("#litragem");
 
+    var $quadro = $(".quadro-itens ul");
+
     this.$btnAdicionar = $("#botao-adicionar");
     this.$btnLimpar = $("#btnLimpar");
     this.$btnCalcular = $("#btnCalcular");
 
     //Funções
     var adicioneItem = function () {
-        alert('adicionar');
+        var card = obtenhaHtmlDoCard();
+        $($quadro).append(card);
     };
 
     var calcular = function () {
@@ -24,12 +27,52 @@ jQuery(document).ready(function($) {
         $elQuantidade.val("");
         $elPreco.val("");
         $elLitragem.val("");
+        $($quadro).html("");
     };
+
+    var obtenhaModelDoCard = function () {
+        return {
+            tipo: $elTipo.val(),
+            nome: $elNome.val(),
+            quantidade: $elQuantidade.val(),
+            preco: $elPreco.val(),
+            litragem: $elLitragem.val()
+        }
+    };
+
+    var obtenhaHtmlDoCard = function () {
+        var model = obtenhaModelDoCard();
+        var card = "<li class='cardFiltro'>";
+        card += "<div class='cardFiltroItem'>";
+        card += "<input type='hidden' data-tipoentidade='3014'/>";
+        card += "<div class='divNome'><i class='fa fa-user'></i><p>"+ model.nome +"</p></div>";
+        card += "<span style='color: #2678C9;background: #E1F3FF' class='excluirCard' title='Remover bebida'></span>";
+        card += "<p class='informacoes'>" + model.quantidade + obtenhaUnidade(model) + model.litragem + " ml</p>";
+        card += "<p class='informacoes'>Preço R$ " + model.preco +"</p>";
+        card += "</div>";
+        card += "</li>";
+
+        return card;
+    };
+
+    var obtenhaUnidade = function (model){
+        if(model.tipo == "Caixa") {
+            return model.quantidade > 1 ? " cxs de " : " cx de ";
+        } else {
+            return model.quantidade > 1 ? " unds de " : " und de ";
+        }
+    };
+
+    var excluaCard = function(e){
+        $(e.target).closest(".cardFiltro").remove();
+        e.stopPropagation();
+    }
 
     //Eventos
     this.$btnAdicionar.on("click", function (e) {
         adicioneItem();
-        e.stopPropagation();
+        //retorno para que não atualize a página devido ao submit
+        return false;
     });
 
     this.$btnLimpar.on("click", function () {
@@ -38,6 +81,9 @@ jQuery(document).ready(function($) {
 
     this.$btnCalcular.on("click", function () {
         limpeTela();
-        e.stopPropagation();
+    });
+
+    $quadro.on("click", ".excluirCard", function (e) {
+        excluaCard(e);
     });
 });
